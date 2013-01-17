0de60db116b468332a264b12848127d5e9f60e34
Dan Prince <dprince@redhat.com>
2013-01-17 15:39:03 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Diffinators
+
+Scripts to generate diff reports for distro packaging branches.
+
+## Description
+
+I use this to generate simple HTML reports which display diffs between
+upstream SmokeStack packaging branches and downstream distro packaging.
+
+## How to run it:
+
+Run the script:
+```bash
+bash fedora.bash
+```
+
+** This generates a report in fedora/out-report/index.html with links to the patch/diffs.
+
+## Copyright
+
+Copyright (c) 2013 Red Hat Inc. See LICENSE.txt for further details.
---
+++ fedora.bash
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+mkdir -p fedora
+cd fedora
+
+#ORIGIN="git@github.com:fedora-openstack"
+ORIGIN="git://github.com/fedora-openstack"
+FEDORA_ORIGIN="git://pkgs.fedoraproject.org"
+OUTDIR="out-report"
+
+mkdir -p $OUTDIR
+
+function generate_diff {
+  echo "Generating diffs..."
+  for PROJ in nova glance keystone swift quantum cinder python-novaclient python-glanceclient python-keystoneclient python-swiftclient python-quantumclient python-cinderclient; do
+
+    echo "processing $PROJ..."
+
+    if [ -d "openstack-$PROJ" ]; then
+      pushd "openstack-$PROJ"
+      git fetch origin
+    else
+      git clone "${ORIGIN}/openstack-${PROJ}.git"
+      pushd "openstack-$PROJ"
+      if [ ${PROJ:0:6} = "python" ]; then
+        git remote add fedoraproject "${FEDORA_ORIGIN}/${PROJ}.git"
+      else
+        git remote add fedoraproject "${FEDORA_ORIGIN}/openstack-${PROJ}.git"
+      fi
+    fi
+
+    git fetch fedoraproject
+
+    git diff fedoraproject/master origin/master > ../$OUTDIR/$PROJ.diff
+    popd
+
+  done
+
+}
+
+function generate_report {
+  echo "Generating report..."
+  pushd $OUTDIR
+
+cat > index.html <<-EOF_CAT
+<html>
+<head>
+  <title>Fedora: upstream packager diffs</title>
+</head>
+<body>
+<h1>Fedora: upstream packager diffs</h1>
+
+<h3>Fedora base URL: $FEDORA_ORIGIN</h3>
+<h3>Upstream base URL: $ORIGIN</h3>
+
+Diffs by size/project:
+<ul>
+EOF_CAT
+
+for DIFF in $(ls *.diff); do
+  LINE_COUNT=$(wc -l $DIFF | cut -f 1 -d " ")
+  echo "<li>$LINE_COUNT: <a href='$DIFF'>$DIFF</a></li>" >> index.html
+done
+
+cat >> index.html <<-EOF_CAT
+</ul>
+
+** Diffs generated with: <i>git diff fedora/master origin/master</i>
+</body>
+</html>
+EOF_CAT
+
+popd
+}
+
+generate_diff
+generate_report
